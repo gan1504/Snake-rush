15b40198b5865f6b12a1a733c022801b27d83f7d
gan1504 <97192034+gan1504@users.noreply.github.com>
2026-04-24 13:24:34 +0200

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,11 @@
-self.addEventListener("install", e => {
-  console.log("Service Worker installed");
+self.addEventListener("install", event => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request));
+self.addEventListener("activate", event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
